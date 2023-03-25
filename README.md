09d679b522a480dedf3ef9916cd674a979c1aa36
Magnus K <magnusku@gmx.de>
2023-03-25 18:24:16 +0100

    made readme pretty

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-Quadtree
 a simple python implementation of a quadtree with pygame visualisation
+
+![example image](https://github.com/github.com/magnusKue/python-Quadtree/main/image.jpg?raw=true)
--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ qTree = qT.Quadtree(pygame.Rect(50,50,800,800), 4)
             rng.randint(0,qTree.bounds.bottomright[0]),
             rng.randint(0,qTree.bounds.bottomright[1])
         )
-    ) for I in range(0)
+    ) for I in range(30)
 ]
 
 while running:
